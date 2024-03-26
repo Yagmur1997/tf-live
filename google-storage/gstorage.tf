52b2a25d3783930cbde85be5464248a412397c83
Aidana Sharip <aidanasharip0797@gmail.com>
2024-03-26 14:41:49 -0500

    123

--- google-storage/gstorage.tf
+++ google-storage/gstorage.tf
@@ -1,6 +1,7 @@
 module "gcloud_storage" {
   source          = "git@github.com:Yagmur1997/tf-modules.git//google-storage"
-  bucket_name     = "statefile-bucket"
+  bucket_name     = "statefile-buckettt"
   bucket_location = "US"
   project_id      = "genuine-plate-417822"
+  level_access    = true
 }
