4ad2ec3445edad6ae998cfed675365165933e6f9
Aidana Sharip <aidanasharip0797@gmail.com>
2024-03-25 19:50:21 -0500

    123

--- gke/qa-cluster.tf
+++ gke/qa-cluster.tf
@@ -1,9 +1,9 @@
 module "qa_gke_cluster" {
-    source = "git@github.com:Yagmur1997/tf-modules.git//gke"
-    service_account_id           = "qa-gke-service-account"
-    service_account_display_name = "QA GKE Service Account"
-    cluster_name                 = "qa-cluster"
-    cluster_location             = "us-central1-b"
-    project_id                   = "genuine-plate-417822"
-    initial_node_count           = 1
+  source                       = "git@github.com:Yagmur1997/tf-modules.git//gke"
+  service_account_id           = "qa-gke-service-account"
+  service_account_display_name = "QA GKE Service Account"
+  cluster_name                 = "qa-cluster"
+  cluster_location             = "us-central1-b"
+  project_id                   = "genuine-plate-417822"
+  initial_node_count           = 1
 }
