e11963eeabd74addb30b3f75f1e2af0bd5b862c0
Aidana Sharip <aidanasharip0797@gmail.com>
2024-03-27 17:48:53 -0500

    changed

--- google-storage/gstorage.tf
+++ google-storage/gstorage.tf
@@ -5,3 +5,17 @@ module "gcloud_storage" {
   project_id      = "genuine-plate-417822"
   level_access    = true
 }
+
+# module "iam_binding" {
+#   source     = "git@github.com:Yagmur1997/tf-modules.git//google-storage"
+#   project_id = "genuine-plate-417822"
+#   # role       = "roles/storage.objectViewer"
+#   # members     = "serviceAccount:aidanasharip0797@gmail.com"
+# }
+
+# module "bucket_iam_binding" {
+#   source      = "git@github.com:Yagmur1997/tf-modules.git//google-storage"
+#   bucket_name = "statefile-buckettt"
+#   # role        = "roles/storage.objectViewer"
+#   # members      = "serviceAccount:aidanasharip0797@gmail.com"
+# }
